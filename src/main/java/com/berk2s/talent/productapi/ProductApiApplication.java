a65ca555f376caa6155df28f49b7e804ab636eee
berk2s <berktopcu@outlook.com>
2021-12-17 20:23:55 +0300

    initial commit

---
+++ src/main/java/com/berk2s/talent/productapi/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.berk2s.talent.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+
+}
